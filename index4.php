dee4b5909f01f507fc4edddb51ed61477545dd87
anas <anasalhiemad@gmail.com>
2024-09-03 11:17:29 +0300

    commit 4

--- index4.php
+++ index4.php
@@ -12,6 +12,7 @@
  * null
  * ===============4
  * search for settype;
+ * result is ::  The settype() function converts a variable to a specific type
  *  */
 
 echo 3 + (int)'4 lessons';
@@ -25,3 +26,15 @@ echo '<br>';
 echo (int)(4.5 + 5.5);
 echo '<br>';
 echo gettype((4.5 + 5.5));
+
+$a = "32"; // string
+echo gettype($a);
+echo '<br>';
+settype($a, "integer"); // $a is now integer
+echo gettype($a);
+
+$b = 32; // integer
+settype($b, "string"); // $b is now string
+
+$c = true; // boolean
+settype($c, "integer"); // $c is now integer (1)
