86b8a8b83b8d694c5b2ef18ee4d82e196a40b015
anas <anasalhiemad@gmail.com>
2024-09-03 11:18:01 +0300

    commit 4

---
+++ assignments.php
@@ -0,0 +1,43 @@
+<!--
+this is comment on
+multiple lines
+on html
+-->
+<?php
+header('Content-Type: text/html; charset=utf-8');?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "My First PHP Page"; ?></title>
+  </head>
+  <body>
+    <div><?php echo 'We Love';?></div>
+    <div><?php print('Elzero Channel'); ?></div>
+  </body>
+</html>
+
+
+
+// ==========================================================================================
+<?php
+// echo 'Prevent Me From Running Please';
+# echo 'Prevent Me From Running Please';
+/* echo 'Prevent Me From Running Please'; */
+?>
+// ==========================================================================================
+
+/* My Application
+ Version 1.0
+ Created By Elzero */
+
+// ==========================================================================================
+//  First Comment
+#  Second Comment
+/* Third Comment */
+// Fourth Comment
+
+
+// ==========================================================================================
---
+++ index 6.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ *= Data Type
+ * ==========
+ * string and escaping
+ * nl2br();
+ *
+ * */
+
+echo 'welcome "php"';
+echo'<br>';
+echo 'welcome \' php\'';
+echo'<br>';
+echo 'i need
+ print on 
+ multiple lines';
+echo'<br>';
+echo nl2br('i need
+ print on 
+ multiple lines');
+echo'<br>';
